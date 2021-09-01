eb2c7b966a90d7351960b58d834f763c0d881c0a
Taylor <taylorwhite21@yahoo.com>
2021-09-01 00:39:44 -0400

    started ticketmaster api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    }
+  };
+
+
+  getEvent = async () => {
+    let events = await axios.get('http://localhost:3001/events');
+    this.setState({
+      events: events.data,
+    })
+    console.log(events);
+  }
+
+  render(){
+    return (
+      <button onClick={this.getEvent}></button>
+    );
+  }
 }
 
 export default App;
